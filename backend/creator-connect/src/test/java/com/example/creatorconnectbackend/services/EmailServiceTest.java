7f6c0efdeb36aa5db56748095dc01a328de6f12e
Kishan Patel <ks255715@dal.ca>
2023-07-15 13:55:53 -0300

    Add test for EmailService and create EmailBody Model

---
+++ backend/creator-connect/src/test/java/com/example/creatorconnectbackend/services/EmailServiceTest.java
@@ -0,0 +1,37 @@
+package com.example.creatorconnectbackend.services;
+
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+
+public class EmailServiceTest {
+
+    private EmailService emailService;
+
+    @Mock
+    private JavaMailSender javaMailSender;
+
+    @BeforeEach
+    public void setUp() {
+        MockitoAnnotations.openMocks(this);
+        emailService = new EmailService(javaMailSender);
+    }
+
+    @Test
+    public void testSendEmail() {
+        String to = "test@example.com";
+        String subject = "Test Subject";
+        String text = "Test Body";
+
+        emailService.sendEmail(to, subject, text);
+
+        verify(javaMailSender).send(any(SimpleMailMessage.class));
+    }
+}
