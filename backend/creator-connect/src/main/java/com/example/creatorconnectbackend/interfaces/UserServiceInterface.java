0f7ad6c4d602df9876c07a3961f4ea9453d40c90
William Brace <wl541831@dal.ca>
2023-07-15 15:30:10 -0300

    Revert "Merge branch 'feature/organizationService' into 'main'"
    This reverts merge request !9

--- backend/creator-connect/src/main/java/com/example/creatorconnectbackend/interfaces/UserServiceInterface.java
+++
@@ -1,8 +0,0 @@
-package com.example.creatorconnectbackend.interfaces;
-
-import com.example.creatorconnectbackend.models.User;
-
-public interface UserServiceInterface {
-	User userRegister(User user);
-    boolean userLogin(User user);
-}
--- backend/creator-connect/src/main/java/com/example/creatorconnectbackend/models/Gender.java
+++
@@ -1,7 +0,0 @@
-package com.example.creatorconnectbackend.models;
-
-public enum Gender {
-    MALE,
-    FEMALE,
-    OTHER;
-}
