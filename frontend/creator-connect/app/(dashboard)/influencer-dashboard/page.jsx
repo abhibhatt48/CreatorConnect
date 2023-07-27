43e299c931db4f5beaa547cdedef0812e431df36
William Brace <wl541831@dal.ca>
2023-07-27 05:26:34 -0300

    adding table

--- frontend/creator-connect/app/(dashboard)/influencer-dashboard/page.jsx
+++ frontend/creator-connect/app/(dashboard)/influencer-dashboard/page.jsx
@@ -1,3 +1,73 @@
+"use client";
+import { Box, Container, Grid, Typography, Tab, Tabs } from "@mui/material";
+import { useState, useEffect } from "react";
+import dynamic from "next/dynamic";
+import axios from "axios";
+import RequestsListTable from "../components/RequestsListTable/RequestsListTable";
+
 export default function InfluencerDashboard() {
-  return <div> InfluencerDashboard </div>;
+  const [requests, setRequests] = useState(null);
+
+  let userData = localStorage.getItem("userData");
+
+  userData = JSON.parse(userData);
+  let userID = userData.userID;
+
+  useEffect(() => {
+    const fetchRequests = async () => {
+      try {
+        const res = await axios.get(
+          "http://localhost:8080/api/connectionReq/influencer/getByID/" + userID
+        );
+        console.log("Response:");
+        setRequests(res.data);
+        console.log(res.data);
+      } catch (error) {
+        console.log("Error:");
+        console.error(error);
+      }
+    };
+
+    fetchRequests();
+  }, []);
+
+  const Placeholder = () => <div>Loading...</div>;
+
+  const RequestCard = dynamic(
+    () => import("../components/RequestCard/RequestCard"),
+    {
+      ssr: false,
+      loading: () => <Placeholder />,
+    }
+  );
+
+  const RecommendedInfluencerCard = dynamic(
+    () =>
+      import(
+        "../components/RecommendedInfluencerCard/RecommendedInfluencerCard"
+      ),
+    {
+      ssr: false,
+      loading: () => <Placeholder />,
+    }
+  );
+
+  return (
+    <Box height="100%" overflow="auto">
+      <Grid container spacing={2} direction="column" mt={7}>
+        <Container maxWidth="lg">
+          <Grid container spacing={2} direction="column">
+            <Grid item xs={12}>
+              <Typography variant="h5" color="#222AEF" fontWeight="600" mt={3}>
+                Influencer List
+              </Typography>
+            </Grid>
+            <Grid item xs={12}>
+              <RequestsListTable requests={requests} />
+            </Grid>
+          </Grid>
+        </Container>
+      </Grid>
+    </Box>
+  );
 }
