df373cdaaf27b27a65fe066121a25772b03f1c25
Vrushank264 <changawala.vrushank26.vc@gmail.com>
2023-07-27 00:48:00 -0300

    Add View Counter code.

---
+++ backend/creator-connect/src/main/java/com/example/creatorconnectbackend/models/ViewCounter.java
@@ -0,0 +1,34 @@
+package com.example.creatorconnectbackend.models;
+
+import java.util.Date;
+
+public class ViewCounter {
+
+    private Long influencerId;
+    private Long orgId;
+    private Date date;
+
+    public Long getInfluencerId() {
+        return influencerId;
+    }
+
+    public void setInfluencerId(Long influencerId) {
+        this.influencerId = influencerId;
+    }
+
+    public Long getOrgId() {
+        return orgId;
+    }
+
+    public void setOrgId(Long orgId) {
+        this.orgId = orgId;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+}
